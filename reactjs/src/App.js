e17a2533736e2a70456e198617b4f8e7eb264a66
Pavel <pavelkurek@seznam.cz>
2021-04-15 18:16:06 +0200

    topka

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          strop ty kulo
-          
+          strop ty kulo zbuzek smrdi
+
         </p>
         <a
           className="App-link"
