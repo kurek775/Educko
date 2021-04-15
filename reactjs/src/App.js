bc5887250892b7148a52dc525e3476b481329c04
Pavel <pavelkurek@seznam.cz>
2021-04-15 18:14:20 +0200

    first strop

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          strop ty kulo
+          
         </p>
         <a
           className="App-link"
