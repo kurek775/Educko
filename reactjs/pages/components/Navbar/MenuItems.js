5bd709625c9955b08e298810c70c5584d029bdf2
Lukas Zbuzek <lukyzbuzek@seznam.cz>
2021-04-30 22:09:41 +0200

    navbar v2

--- reactjs/pages/components/Navbar/MenuItems.js
+++ reactjs/pages/components/Navbar/MenuItems.js
@@ -2,26 +2,21 @@ export default [
   {
     id: 1,
     title: "O nás",
-    cName: "nav-link",
   },
   {
     id: 2,
     title: "Služby",
-    cName: "nav-link",
   },
   {
     id: 3,
     title: "Kontakt",
-    cName: "nav-link",
   },
   {
     id: 4,
     title: "Contact",
-    cName: "nav-link",
   },
   {
     id: 5,
     title: "Feedback",
-    cName: "nav-link",
   },
 ];
--- reactjs/pages/components/Navbar/Navbar.js
+++ reactjs/pages/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import MenuItems from "./MenuItems";
-import "../../../styles/NavStyle.module.css";
+import NavStyle from "../../../styles/NavStyle.module.css";
 
 const Navbar = props => {
   const [click, setClick] = React.useState();
@@ -10,19 +10,19 @@ const Navbar = props => {
     setClick(!click);
   };
   return (
-    <nav className="nav">
-      <div className="nav-center container">
-        <h1 className="logo">Edučko</h1>
+    <nav className={NavStyle.nav}>
+      <div className={NavStyle.nav-center.container}>
+        <h1 className={NavStyle.logo}>Edučko</h1>
 
-        <ul className={click ? "nav-list active" : "nav-list"}>
-          {MenuItems.map(({ id, title, cName }) => (
-            <li key={id} className="nav-item">
-              <div className={cName}>{title}</div>
+        <ul className={click ? NavStyle.nav-list active : NavStyle.nav-list}>
+          {MenuItems.map(({ id, title}) => (
+            <li key={id} className={NavStyle.nav-item}>
+              <div className={NavStyle.nav-link}>{title}</div>
             </li>
           ))}
         </ul>
 
-        <div className="hamburger" onClick={clickHandler}>
+        <div className={NavStyle.hamburger} onClick={clickHandler}>
           {click ? <FaTimes /> : <FaBars />}
         </div>
       </div>
