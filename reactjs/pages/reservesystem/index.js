4c3b22843fcd1d69c929ad287dec4cf8c1f02cff
Pavel <pavelkurek@seznam.cz>
2021-05-12 17:48:42 +0200

    rezervsyst

--- reactjs/pages/reservesystem/index.js
+++ reactjs/pages/reservesystem/index.js
@@ -1,12 +1,17 @@
 
-import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda } from "@syncfusion/ej2-react-schedule";
+import { Inject,ScheduleComponent,WorkWeek,ViewsDirective, ViewDirective } from "@syncfusion/ej2-react-schedule";
 
 function reservesystem (){
 
     return(
 
-        <ScheduleComponent>
-        <Inject services = {[Day, Week, WorkWeek, Month, Agenda] }></Inject>
+        <ScheduleComponent   timeFormat="HH:mm" >
+        <Inject services = {[WorkWeek] }></Inject>
+        <ViewsDirective>
+ 
+        <ViewDirective option='WorkWeek'/>
+ 
+    </ViewsDirective>
         </ScheduleComponent>
     )
 }
