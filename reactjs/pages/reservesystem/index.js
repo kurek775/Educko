a7ed0732680b81cfcc30b28beb6dc957440550bc
Pavel <pavelkurek@seznam.cz>
2021-05-22 22:33:51 +0200

    uz nevim co

--- reactjs/pages/reservesystem/index.js
+++ reactjs/pages/reservesystem/index.js
@@ -2,7 +2,29 @@
 import { Inject,ScheduleComponent,WorkWeek,ViewsDirective, ViewDirective } from "@syncfusion/ej2-react-schedule";
 
 function reservesystem (){
-
+/**
+ * L10n modules provides localized text for different culture.
+ * ```typescript
+ * import {setCulture} from '@syncfusion/ts-base-library';
+ * //load global locale object common for all components.
+ * L10n.load({
+ *    'fr-BE': {
+ *       'button': {
+ *            'check': 'vérifié'
+ *        }
+ *    }
+ * });
+ * //set globale default locale culture.
+ * setCulture('fr-BE');
+ * let instance: L10n = new L10n('button', {
+ *    check: 'checked'
+ * });
+ * //Get locale text for current property.
+ * instance.getConstant('check');
+ * //Change locale culture in a component.
+ * instance.setLocale('en-US');
+ * ```
+ */
     return(
 
         <ScheduleComponent   timeFormat="HH:mm" >
@@ -17,3 +39,4 @@ function reservesystem (){
 }
 
 export default reservesystem;
+
