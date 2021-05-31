b3c2770e6d46122d86289f3778e404f89725149a
Pavel <pavelkurek@seznam.cz>
2021-05-31 19:32:36 +0200

    ee

--- reactjs/pages/reservesystem/index.js
+++ reactjs/pages/reservesystem/index.js
@@ -6,49 +6,6 @@ import {
   ViewDirective,
 } from "@syncfusion/ej2-react-schedule";
 
-<<<<<<< HEAD
-import { Inject,ScheduleComponent,WorkWeek,ViewsDirective, ViewDirective } from "@syncfusion/ej2-react-schedule";
-
-function reservesystem (){
-/**
- * L10n modules provides localized text for different culture.
- * ```typescript
- * import {setCulture} from '@syncfusion/ts-base-library';
- * //load global locale object common for all components.
- * L10n.load({
- *    'fr-BE': {
- *       'button': {
- *            'check': 'vérifié'
- *        }
- *    }
- * });
- * //set globale default locale culture.
- * setCulture('fr-BE');
- * let instance: L10n = new L10n('button', {
- *    check: 'checked'
- * });
- * //Get locale text for current property.
- * instance.getConstant('check');
- * //Change locale culture in a component.
- * instance.setLocale('en-US');
- * ```
- */
-    return(
-
-        <ScheduleComponent   timeFormat="HH:mm" >
-        <Inject services = {[WorkWeek] }></Inject>
-        <ViewsDirective>
- 
-        <ViewDirective option='WorkWeek'/>
- 
-    </ViewsDirective>
-        </ScheduleComponent>
-    )
-}
-
-export default reservesystem;
-
-=======
 function reservesystem() {
   return (
     <ScheduleComponent timeFormat="HH:mm">
@@ -61,4 +18,3 @@ function reservesystem() {
 }
 
 export default reservesystem;
->>>>>>> 31cca41467a59adae8002fd79425df421b133878
