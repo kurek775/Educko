6fec61e57df6d9af1d50f19746d10fc7c3f9ae6f
Pavel <pavelkurek@seznam.cz>
2021-06-01 21:33:56 +0200

    carousel+bootstrap in

--- reactjs/components/Navbar/Carousel.js
+++ reactjs/components/Navbar/Carousel.js
@@ -1,24 +1,54 @@
-import { Carousel } from 'react-responsive-carousel';
-
-
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from "./Footer.module.css";
 
 const Imagecarousel = () => {
   
-    <Carousel>
-  <div>
-                    <img src="https://blog.inpage.cz/obrazek/3/kitten-jpg/" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="https://blog.inpage.cz/obrazek/3/kitten-jpg/" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="https://blog.inpage.cz/obrazek/3/kitten-jpg/" />
-                    <p className="legend">Legend 3</p>
-                </div>
-  </Carousel>
-   
-};
+return (
+<div className={classes.dov}>
+
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://www.tasteofhome.com/wp-content/uploads/2018/07/back-to-school-excitement-GettyImages-908815432.jpg"
+      alt="First slide"
+      sizes="80vh"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img 
+      className="d-block w-100"
+      src="https://img.etimg.com/thumb/msid-77391367,width-650,imgsize-677864,,resizemode-4,quality-100/to-reduce-risk-experts-say-schools-should-make-adjustments-when-resuming-in-person-classes-.jpg"
+      alt="Second slide"
+      sizes="80vh"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://media.vanityfair.com/photos/5f64eda08f3d1388648dde56/master/pass/covid-classrooms.jpg"
+      alt="Third slide"
+      sizes="80vh"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
+)  
+}
 
 export default Imagecarousel;
--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Imagecarousel from "../components/Navbar/Carousel";
-import ImageSlider from "../components/Navbar/Slider";
-import { SliderData } from "../components/Navbar/SliderData";
+
 
 import classes from "../styles/homePage.module.css";
 
 export default function Home() {
   return (
     <div className={classes.section}>
-<ImageSlider slides={SliderData}></ImageSlider>
+<Imagecarousel></Imagecarousel>
     </div>
   );
 }
