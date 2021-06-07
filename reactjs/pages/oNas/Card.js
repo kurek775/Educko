ece9074e64cb4b069726220ab16995c272830485
Matěj Kratochvíl <68486535+kurzmoment@users.noreply.github.com>
2021-06-07 20:16:50 +0200

    ff

--- reactjs/pages/oNas/Card.js
+++ reactjs/pages/oNas/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Card.module.css";
-import Image from "next/image";
+// import Image from "next/image";
 
 function Card({ img, imgLabel, name, content }) {
   return (
