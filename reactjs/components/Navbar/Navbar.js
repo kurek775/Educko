af4a3b6b60586dc8f049b057f918be4d378fe3b5
Pavel <pavelkurek@seznam.cz>
2021-06-11 21:38:55 +0200

    Navbar

--- reactjs/components/Navbar/Navbar.js
+++ reactjs/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useSession, signOut } from "next-auth/client";
 
-import NavStyle from "./NavStyle.module.css";
+import { useSession, signOut } from "next-auth/client";
+import{ Navbar, Container, Nav} from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Link from "next/link";
 import Login_button from "./Login_button";
 
-const Navbar = (props) => {
+const Naavbar = (props) => {
   const [click, setClick] = useState();
   const [session, loading] = useSession();
   const clickHandler = () => {
@@ -15,144 +16,115 @@ const Navbar = (props) => {
 
   function logoutHandler() {
     signOut();
-  }
-
+  } 
   if (loading) {
     return <p>Loading...</p>;
   }
-  return (
-    <header>
-      <nav className={NavStyle.nav}>
-        <div className={NavStyle.navCenter}>
-          <h1>
-            <Link href="/">
-              <a className={NavStyle.logo}>Edučko</a>
-            </Link>
-          </h1>
+return (
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+    <Link href="/">
 
-          <ul className={click ? NavStyle.navListActive : NavStyle.navList}>
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <p>{session.user.name}</p>
-                </div>
-              </li>
+    <Navbar.Brand >EDUCKO</Navbar.Brand>
+
+    </Link>
+ 
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+    {session && (<Nav.Link>
+             {session.user.name}
+             </Nav.Link>
             )}
 
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/platba">
-                    <a className={NavStyle.button}>Platba</a>
-                  </Link>
-                </div>
-              </li>
+{session && (<Nav.Link><Link href="/platba">
+             Platba
+           </Link>  </Nav.Link>
             )}
 
-            <li className={NavStyle.navItem}>
-              <div className={NavStyle.navLink}>
-                <Link href="/oNas">
-                  <a className={NavStyle.button}>O nás</a>
-                </Link>
-              </div>
-            </li>
-            <li className={NavStyle.navItem}>
-              <div className={NavStyle.navLink}>
-                <Link href="/contacts">
-                  <a className={NavStyle.button}>Kontakty</a>
-                </Link>
-              </div>
-            </li>
+{session && (<Nav.Link><Link href="/kalendar-rezervaci">
+Rezervacni system
+            </Link> </Nav.Link>
+            )}
 
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/kalendar-rezervaci">
-                    <a className={NavStyle.button}>Rezervacni system</a>
-                  </Link>
-                </div>
-              </li>
+{session && (<Nav.Link><Link href="/profile">
+Profil
+            </Link> </Nav.Link>
             )}
-            {!session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Login_button></Login_button>
-                </div>
-              </li>
-            )}
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/profile">
-                    <a className={NavStyle.button}>Profil</a>
-                  </Link>
-                </div>
-              </li>
-            )}
-            {session && session.user.name === "admin" && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/vytvareni-lektora">
-                    <a className={NavStyle.button}>Vytvoření lektora</a>
-                  </Link>
-                </div>
-              </li>
-            )}
-            {session && session.user.name === "admin" && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/vytvareni-predmetu">
-                    <a className={NavStyle.button}>Vytvoření předmětu</a>
-                  </Link>
-                </div>
-              </li>
-            )}
-            {session && session.user.image === "lector" && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href={"/vytvoreni-hodiny"}>
-                    <a className={NavStyle.button}>Vytvoreni hodiny</a>
-                  </Link>
-                </div>
-              </li>
-            )}
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href={"hodiny"}>
-                    <a className={NavStyle.button}>Hodiny</a>
-                  </Link>
-                </div>
-              </li>
-            )}
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/seznam-lektoru">
-                    <a className={NavStyle.button}>Seznam lektorů</a>
-                  </Link>
-                </div>
-              </li>
-            )}
-            {session && (
-              <li className={NavStyle.navItem}>
-                <div className={NavStyle.navLink}>
-                  <Link href="/">
-                    <a className={NavStyle.button} onClick={logoutHandler}>
-                      Odhlásit se
-                    </a>
-                  </Link>
-                </div>
-              </li>
-            )}
-          </ul>
-          <div className={NavStyle.hamburger} onClick={clickHandler}>
-            {click ? <FaTimes /> : <FaBars />}
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
-};
 
-export default Navbar;
+{session && session.user.name === "admin" && (
+  <Nav.Link> <Link href="/vytvareni-lektora">
+Vytvoření lektora
+            </Link> </Nav.Link>
+            )}
+{session && session.user.name === "admin" && (
+  <Nav.Link> <Link href="/vytvareni-predmetu">
+Vytvoření předmětu
+            </Link> </Nav.Link>
+            )}
+
+{session && session.user.image === "lector" && (
+  <Nav.Link> <Link href="/vytvoreni-hodiny">
+Vytvoření hodiny
+            </Link> </Nav.Link>
+            )}
+{session && (<Nav.Link><Link href="hodiny">
+Hodiny
+            </Link> </Nav.Link>
+            )}
+{session && (<Nav.Link><Link href="/seznam-lektoru">
+Hodiny
+            </Link> </Nav.Link>
+            )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<Nav.Link>
+
+    <Link href="/oNas">
+  
+     O nás
+      </Link>
+      </Nav.Link>
+
+      <Nav.Link>
+      <Link href="/contacts">
+     Kontakty
+      </Link></Nav.Link>
+ 
+    </Nav>
+    <Nav>
+
+
+    {!session && (
+      <Nav.Link>      <Login_button></Login_button></Nav.Link>
+    )}
+
+{session && (<Nav.Link><Link href="/">
+<a onClick={logoutHandler}> Odhlásit se </a>
+            </Link> </Nav.Link>
+            )}
+
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+)
+}
+export default Naavbar;
