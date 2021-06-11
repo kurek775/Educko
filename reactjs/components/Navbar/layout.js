62cbf8e92c67e6284745191578fe4239c85b7d5a
Pavel <pavelkurek@seznam.cz>
2021-06-11 20:09:00 +0200

    top

--- reactjs/components/Navbar/layout.js
+++ reactjs/components/Navbar/layout.js
@@ -2,13 +2,13 @@ import { Fragment } from "react";
 import Navbar from "./Navbar";
 import Background from "../background-animace/background";
 import Footer from "./Footer";
-import Naavbar from "./Naavbar";
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Layout(props) {
   return (
     <Fragment>
-          <Naavbar></Naavbar>
+    
       <Navbar></Navbar>
 
       <main>{props.children}</main>
